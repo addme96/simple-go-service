3d056116906cd04d1fd268c3bacd94ebfda45282
Dawid Dudek <addme96@gmail.com>
2022-06-19 23:13:37 +0200

    added cors

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/cors v1.2.1
 	github.com/golang/mock v1.6.0
 	github.com/jackc/pgconn v1.12.0
 	github.com/jackc/pgx/v4 v4.16.0
--- simple-service/main.go
+++ simple-service/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/addme96/simple-go-service/simple-service/repositories"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/jackc/pgx/v4"
 )
 
@@ -26,13 +27,20 @@ const (
 
 func main() {
 	db := database.NewDB(adapters.Pgx(pgx.Connect), getConnectionString())
-	ctx := context.Background()
-
-	if err := db.Seed(ctx); err != nil {
+	if err := db.Seed(context.Background()); err != nil {
 		panic(err)
 	}
 	resourceHandler := handlers.NewResource(repositories.NewResource(db))
 	r := chi.NewRouter()
+	// Basic CORS. For more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
